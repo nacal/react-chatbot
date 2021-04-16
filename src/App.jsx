5629e5be25ce15ca8c6c2c611b460cde5af623ec
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-04-16 16:54:47 +0900

    Develop forms entry point

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './assets/styles/style.scss';
 import defaultDataset from "./dataset"
 import {AnswersList} from "./components"
 import {Chats} from "./components"
-import FormDialog from "./components/Forms/FormDialog"
+import {FormDialog} from "./components/Forms/index"
 
 export default class App extends React.Component {
   constructor(props) {
